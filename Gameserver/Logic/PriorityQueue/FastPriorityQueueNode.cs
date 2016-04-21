37440707e0300a424881776347e4fc2487a04267
Fro-Z <fro-z@email.cz>
2016-04-21 23:26:40 +0200

    Added priority queue (used for pathfinding)
    https://github.com/BlueRaja/High-Speed-Priority-Queue-for-C-Sharp/
    (MIT License)

---
+++ Gameserver/Logic/PriorityQueue/FastPriorityQueueNode.cs
@@ -0,0 +1,22 @@
+﻿namespace Priority_Queue
+{
+    public class FastPriorityQueueNode
+    {
+        /// <summary>
+        /// The Priority to insert this node at.  Must be set BEFORE adding a node to the queue
+        /// </summary>
+        public double Priority { get; set; }
+
+        /// <summary>
+        /// <b>Used by the priority queue - do not edit this value.</b>
+        /// Represents the order the node was inserted in
+        /// </summary>
+        public long InsertionIndex { get; set; }
+
+        /// <summary>
+        /// <b>Used by the priority queue - do not edit this value.</b>
+        /// Represents the current position in the queue
+        /// </summary>
+        public int QueueIndex { get; set; }
+    }
+}
