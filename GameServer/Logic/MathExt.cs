8d9c2517dff179473cf9c01a3d247c44c0cbcecb
Fro-Z <fro-z@email.cz>
2016-04-21 23:30:31 +0200

    Added math functions needed for pathfinding

---
+++ GameServer/Logic/MathExt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueSandbox.GameServer.Logic
+{
+    /// <summary>
+    /// Math extensions
+    /// </summary>
+    public static class MathExt
+    {
+        public const float FP_TOLERANCE = 10e-10f;
+
+        public static float Vec2Cross(Vector2 first, Vector2 second)
+        {
+            return first.X * second.Y - second.X * first.Y;
+        }
+
+        public static bool FloatEqual(float a, float b, float tolerance = FP_TOLERANCE)
+        {
+            return (Math.Abs(a - b) <= tolerance);
+        }
+
+        public static bool VectorEqual(Vector2 a, Vector2 b)
+        {
+            return FloatEqual(a.X, b.X) && FloatEqual(a.Y, b.Y);
+        }
+    }
+}
