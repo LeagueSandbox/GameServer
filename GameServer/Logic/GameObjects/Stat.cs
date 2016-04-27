ba97281717b02dea20b57edadca9b6a41e5aabd2
coquicox <nicolasroy96@gmail.com>
2016-04-27 15:41:58 +0100

    Added IBuff interface and Stat with tests

---
+++ GameServer/Logic/GameObjects/Stat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueSandbox.GameServer.Logic.GameObjects
+{
+    public class Stat
+    {
+        public float ModifiedBase { get; set; }
+        public float Bonus { get; set; }
+        public float Total { get { return BaseValue + ModifiedBase + Bonus; } }
+        public float BaseValue { get; set; }
+
+        public Stat(float baseValue, float modifierBase, float bonus)
+        {
+            BaseValue = baseValue;
+            ModifiedBase = modifierBase;
+            Bonus = bonus;
+        }
+
+        public Stat() : this(0, 0, 0)
+        {
+            
+        }
+   }
+}
