14ea001e96f2d2d8170bc4d7bc1daf9a0aec028d
joaquin95 <joaquin95uru@gmail.com>
2016-05-05 11:07:51 -0300

    Works when the game has more than one player

--- GameServer/Logic/Packets/PacketHandlers/Handlers/HandleStartGame.cs
+++ GameServer/Logic/Packets/PacketHandlers/Handlers/HandleStartGame.cs
@@ -38,19 +38,34 @@ namespace LeagueSandbox.GameServer.Core.Logic.PacketHandlers.Packets
 
             if (game.isStarted())
             {
+                var map = game.getMap();
+                if (peerInfo.Disconnected)
+                {
+                    foreach (var Player in game.getPlayers())
+                    {
+                        if (Player.Item2.getTeam() == peerInfo.getTeam())
+                        {
+                            var HeroSpawn2Packet = new HeroSpawn2(Player.Item2.getChampion());
+                            PacketHandlerManager.getInstace().sendPacket(peer, HeroSpawn2Packet, Channel.CHL_S2C);
+                        }
+                    }
+                    peerInfo.Disconnected = false;
+                    PacketNotifier.notifyDebugMessage("Player " + peerInfo.userId + " reconnected.");
+
+                    // Send the initial game time sync packets, then let the map send another
+                    float gameTime = map.getGameTime() / 1000.0f;
+
+                    var timer = new GameTimer(gameTime); // 0xC1
+                    PacketHandlerManager.getInstace().sendPacket(peerInfo.getPeer(), timer, Channel.CHL_S2C);
+
+                    var timer2 = new GameTimerUpdate(gameTime); // 0xC2
+                    PacketHandlerManager.getInstace().sendPacket(peerInfo.getPeer(), timer2, Channel.CHL_S2C);
+                    return true;
+                }
+
                 foreach (var p in game.getPlayers())
                 {
-                    var map = game.getMap();
-                    if (!p.Item2.Disconnected)
-                    {
-                        map.AddObject(p.Item2.getChampion());
-                    }
-                    else
-                    {
-                        PacketNotifier.notifyChampionSpawned(p.Item2.getChampion(), p.Item2.getChampion().getTeam());
-                        p.Item2.Disconnected = false;
-                        PacketNotifier.notifyDebugMessage("Player " + game.getPeerInfo(peer).userId + " reconnected.");
-                    }
+                    map.AddObject(p.Item2.getChampion());
 
                     // Send the initial game time sync packets, then let the map send another
                     float gameTime = map.getGameTime() / 1000.0f;
