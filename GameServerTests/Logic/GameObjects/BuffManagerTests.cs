2ca5f824cfb405a5f43ddf2e6401c241941c612b
joaquin95 <joaquin95uru@gmail.com>
2016-05-17 01:39:05 -0300

    Added test

---
+++ GameServerTests/Logic/GameObjects/BuffManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeagueSandbox.GameServer.Logic.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameServerTests;
+
+namespace LeagueSandbox.GameServer.Logic.GameObjects.Tests
+{
+    [TestClass()]
+    public class BuffManagerTests
+    {
+        [TestMethod()]
+        public void RemoveBuffTest()
+        {
+            var game = new TestHelpers.DummyGame();
+            game.LoadBuffManager();
+
+            var result = game.BuffManager.RemoveBuff("Haste");
+            Assert.IsFalse(result);
+        }
+    }
+}
