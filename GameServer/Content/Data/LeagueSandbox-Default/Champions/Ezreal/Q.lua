fc403a31785f67159cfa1f2989653be7033ca4f8
FurkanS1821 <furkan.sam.1821@gmail.com>
2016-08-04 16:44:54 +0300

    Ezreal Kit is working nearly completely
    Exluding passive, W attackspeed buff on allies and R damage reduction per target hit.

--- GameServer/Content/Data/LeagueSandbox-Default/Champions/Ezreal/Q.lua
+++ GameServer/Content/Data/LeagueSandbox-Default/Champions/Ezreal/Q.lua
@@ -7,22 +7,16 @@ function onFinishCasting()
     local trueCoords = current + range
 
     addProjectile("EzrealMysticShotMissile", trueCoords.x, trueCoords.y)
-	printChat("You used Q");
-	
-	-- This buff isnt in the main servers
-    -- addBuff("Haste", 10.0, 1, getOwner(), getOwner())
-
 end
 
 function applyEffects()
-    local attackDamage = getOwner():GetStats().AttackDamage
-    local abilityPower = getOwner():GetStats().AbilityPower
-    local damage = getEffectValue(0) + 1.1 * attackDamage.Total + (0.4 * abilityPower.Total)
+    local AD = getOwner():GetStats().AttackDamage.Total*1.1
+    local AP = getOwner():GetStats().AbilityPower.Total*0.4
+    local damage = getEffectValue(0) + AD + AP
     dealPhysicalDamage(damage)
 	
-	
-    -- TODO this can be fetched from projectile inibin "HitEffectName"
-    addParticleTarget("Ezreal_mysticshot_tar.troy", getTarget())
+	-- TODO this can be fetched from projectile inibin "HitEffectName"
+    addParticleTarget(getOwner(), "Ezreal_mysticshot_tar.troy", getTarget())
 
     destroyProjectile()
 end
--- GameServer/Content/Data/LeagueSandbox-Default/Champions/Ezreal/W.lua
+++ GameServer/Content/Data/LeagueSandbox-Default/Champions/Ezreal/W.lua
@@ -6,21 +6,12 @@ function onFinishCasting()
     local range = to * 1000
     local trueCoords = current + range
 	
-	-- "EzrealMysticShotMissile" is not the name, if someone knows it please write it
-    addProjectile("EzrealEssenceFluxMissile", trueCoords.x, trueCoords.y);
-	printChat("You used W");
+    addProjectile("EzrealEssenceFluxMissile", trueCoords.x, trueCoords.y)
 end
 
 function applyEffects()
-
-    local abilityPower = getOwner():GetStats().AbilityPower
-    local damage = getEffectValue(0) + 0.8 * abilityPower.Total
-    dealPhysicalDamage(damage)
-	
-    -- TODO this can be fetched from projectile inibin "HitEffectName"
-	
-	-- This is not the name of the particle, if someone knows it please write it
-	addParticleTarget("Ezreal_essenceflux_tar.troy", getTarget())
-
-    destroyProjectile()
+    if getOwner():getTeam() ~= getTarget():getTeam() then
+        dealMagicalDamage(25+getSpellLevel()*45+getOwner():GetStats().AbilityPower.Total*0.8)
+	    addParticleTarget(getOwner(), "Ezreal_essenceflux_tar.troy", getTarget())
+    end
 end
