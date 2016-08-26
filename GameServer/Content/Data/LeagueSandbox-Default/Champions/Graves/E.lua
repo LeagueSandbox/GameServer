703f7a0b23ddcd7d9cd122fa3a50ce109788d2a3
FurkanS1821 <furkan.sam.1821@gmail.com>
2016-08-27 02:30:26 +0300

    A little fix

--- GameServer/Content/Data/LeagueSandbox-Default/Champions/Graves/E.lua
+++ GameServer/Content/Data/LeagueSandbox-Default/Champions/Graves/E.lua
@@ -1,14 +1,13 @@
 Vector2 = require 'Vector2' -- include 2d vector lib 
 
 function onFinishCasting()
-    addParticleTarget("Graves_Move_OnBuffActivate.troy", getOwner())
- 
     local current = Vector2:new(getOwnerX(), getOwnerY())
     local to = (Vector2:new(getSpellToX(), getSpellToY()) - current):normalize()
     local range = to * 425
     local trueCoords = current + range
     
     dashTo(getOwner(), trueCoords.x, trueCoords.y, 1200, 0, "Spell3")
+    addParticleTarget(getOwner(), "Graves_Move_OnBuffActivate.troy", getOwner())
 end
 
 function applyEffects()
