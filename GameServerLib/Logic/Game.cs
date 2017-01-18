3348c273ba8e5af28a35bdfb4af660efc33185ff
Chewy <chewybomber@gmail.com>
2017-01-18 18:04:05 +0100

    Progress #417 -- Basics for old TT support

--- GameServerLib/Logic/Game.cs
+++ GameServerLib/Logic/Game.cs
@@ -109,6 +109,7 @@ namespace LeagueSandbox.GameServer.Core.Logic
             var dic = new Dictionary<string, Type>
             {
                 { "LeagueSandbox-Default-Map1", typeof(SummonersRift) },
+                { "LeagueSandbox-Default-Map4", typeof(OriginalTwistedTreeline) },
                 // { "LeagueSandbox-Default-Map8", typeof(CrystalScar) },
                 { "LeagueSandbox-Default-Map10", typeof(TwistedTreeline) },
                 // { "LeagueSandbox-Default-Map11", typeof(NewSummonersRift) },
