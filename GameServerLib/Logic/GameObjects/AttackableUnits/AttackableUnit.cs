0145edfdd8ab4ecf90e419bcb6cbfd3dd7d65920
moonshadow565 <moonshadow565@gmail.com>
2017-04-27 15:11:12 +0200

    Setup base object classes

---
+++ GameServerLib/Logic/GameObjects/AttackableUnits/AttackableUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueSandbox.GameServer.Logic.GameObjects
+{
+    public class AttackableUnit : Unit
+    {
+        public AttackableUnit(string model, Stats stats, int collisionRadius = 40,
+            float x = 0, float y = 0, int visionRadius = 0, uint netId = 0) : 
+            base(model, stats, collisionRadius, x, y, visionRadius, netId)
+        {
+        }
+    }
+}
---
+++ GameServerLib/Logic/GameObjects/Missiles/ObjMissile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueSandbox.GameServer.Logic.GameObjects
+{
+    public class ObjMissile : GameObject
+    {
+        public ObjMissile(float x, float y, int collisionRadius, int visionRadius = 0, uint netId = 0) :
+            base(x, y, collisionRadius, visionRadius, netId)
+        {
+        }
+    }
+}
