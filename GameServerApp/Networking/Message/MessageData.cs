d526422c2eeb594de6fa098665364e03aead63ac
MatthewFrench <matthewf@ksu.edu>
2018-08-19 23:49:24 -0500

    Added networking stuff.

---
+++ GameServerApp/Networking/Message/MessageData.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Soly.ByteArray;
+namespace ROIDForumServer
+{
+	public abstract class MessageData
+	{
+		public abstract void AddToByteData(ByteArray byteData, int loc);
+		public abstract uint GetLength();
+	}
+
+	public class MessageDataUint8 : MessageData
+	{
+		byte value;
+
+		public MessageDataUint8(byte value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+			byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 1;
+		}
+	}
+
+
+
+	public class MessageDataInt8 : MessageData
+	{
+		sbyte value;
+
+		public MessageDataInt8(sbyte value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 1;
+		}
+	}
+    
+	public class MessageDataUint16 : MessageData
+	{
+		ushort value;
+
+		public MessageDataUint16(ushort value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 2;
+		}
+	}
+    
+	public class MessageDataInt16 : MessageData
+	{
+		short value;
+
+		public MessageDataInt16(short value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 2;
+		}
+	}
+
+	public class MessageDataUint32 : MessageData
+	{
+		uint value;
+
+		public MessageDataUint32(uint value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 4;
+		}
+	}
+
+	public class MessageDataInt32 : MessageData
+	{
+		int value;
+
+		public MessageDataInt32(int value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 4;
+		}
+	}
+
+	public class MessageDataFloat32 : MessageData
+	{
+		float value;
+
+		public MessageDataFloat32(float value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byte[] bytes = BitConverter.GetBytes(this.value);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            
+            byteData.Write(bytes, 0, (int)GetLength(), loc);
+		}
+
+		public override uint GetLength()
+		{
+			return 4;
+		}
+	}
+
+	public class MessageDataFloat64 : MessageData
+	{
+		double value;
+
+		public MessageDataFloat64(double value)
+		{
+			this.value = value;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            byteData.Write(this.value, loc, Endianess.BigEndian);
+		}
+
+		public override uint GetLength()
+		{
+			return 8;
+		}
+	}
+
+	public class MessageDataString : MessageData
+	{
+		byte[] value;
+		readonly uint totalLength;
+
+		public MessageDataString(string value)
+		{
+			this.value = System.Text.Encoding.Unicode.GetBytes(value);
+			//Total length is buffer plus length of buffer
+			this.totalLength = 4 + (uint)this.value.Length;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+			//Write total length (total length + string length)
+			byteData.Write(this.totalLength, loc, Endianess.BigEndian);
+            //Write string
+			byteData.Write(value, 0, value.Length, loc + 4);
+		}
+        
+		public override uint GetLength()
+		{
+			return this.totalLength;
+		}
+	}
+
+	public class MessageDataBinary : MessageData
+	{
+		byte[] value;
+		readonly uint totalLength;
+
+		public MessageDataBinary(byte[] value)
+		{
+			this.value = value;
+			//Total length is buffer plus length of buffer
+			this.totalLength = 4 + (uint)this.value.Length;
+		}
+
+		public override void AddToByteData(ByteArray byteData, int loc)
+		{
+            //Write total length (total length + string length)
+            byteData.Write(this.totalLength, loc, Endianess.BigEndian);
+            //Write string
+            byteData.Write(value, 0, value.Length, loc + 4);
+		}
+
+		public override uint GetLength()
+		{
+			return this.totalLength;
+		}
+	}
+}
---
+++ GameServerApp/Networking/ServerMessages.cs
@@ -0,0 +1,189 @@
+﻿using System;
+namespace ROIDForumServer
+{
+    public class ServerMessages
+    {
+        public enum Controller
+        {
+            Chat = 0,
+            Login = 1,
+            Section = 2
+        }
+
+        public enum ChatMsg
+        {
+            Msg = 0,
+            OnlineList = 1
+        }
+
+        public enum SectionMsg {
+            AllThreads = 0,
+            AddThread = 1,
+            RemoveThread = 2,
+            UpdateThread = 3,
+            MoveThreadToTop = 4,
+            AddComment = 5,
+            RemoveComment = 6,
+            UpdateComment = 7
+        }
+
+        public enum LoginMsg {
+            GetAvatar = 0,
+            LoginFailed = 1,
+            LoggedOut = 2,
+            RegisterFailed = 3,
+            LoggedIn = 4,
+        }
+
+        public static byte[] LoggedInMessage(String username, String password)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Login);
+            message.AddUint8((byte)LoginMsg.LoggedIn);
+            message.AddString(username);
+            message.AddString(password);
+
+            return message.ToBuffer();
+        }
+
+        public static byte[] RegisterFailedMessage()
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Login);
+            message.AddUint8((byte)LoginMsg.RegisterFailed);
+            return message.ToBuffer();
+        }
+
+        public static byte[] LoggedOutMessage()
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Login);
+            message.AddUint8((byte)LoginMsg.LoggedOut);
+            return message.ToBuffer();
+        }
+
+        public static byte[] LoginFailedMessage()
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Login);
+            message.AddUint8((byte)LoginMsg.LoginFailed);
+            return message.ToBuffer();
+        }
+
+        public static byte[] GetAvatarMessage(String avatarURL)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Login);
+            message.AddUint8((byte)LoginMsg.GetAvatar);
+            message.AddString(avatarURL);
+            return message.ToBuffer();
+        }
+
+        public static byte[] AllThreadsMessage(SectionController controller)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.AllThreads);
+            message.AddString(controller.name);
+            message.AddUint32((uint)controller.threadController.threads.Count);
+            for (int i = 0; i < controller.threadController.threads.Count; i++)
+            {
+                ThreadInfo t = controller.threadController.threads[i];
+                message.AddBinary(t.toBinary());
+            }
+            return message.ToBuffer();
+        }
+
+        public static byte[] AddThreadMessage(SectionController controller, ThreadInfo thread) {
+
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.AddThread);
+            message.AddString(controller.name);
+            message.AddBinary(thread.toBinary());
+            return message.ToBuffer();
+        }
+
+        public static byte[] RemoveThreadMessage(SectionController controller, ThreadInfo thread)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.RemoveThread);
+            message.AddString(controller.name);
+            message.AddUint32((uint)thread.id);
+            return message.ToBuffer();
+        }
+
+        public static byte[] UpdateThreadMessage(SectionController controller, ThreadInfo thread)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.UpdateThread);
+            message.AddString(controller.name);
+            message.AddUint32((uint)thread.id);
+            message.AddString(thread.title);
+            message.AddString(thread.description);
+            return message.ToBuffer();
+        }
+
+        public static byte[] MoveToTopThreadMessage(SectionController controller, ThreadInfo thread)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.MoveThreadToTop);
+            message.AddString(controller.name);
+            message.AddUint32((uint)thread.id);
+            return message.ToBuffer();
+        }
+
+        public static byte[] AddCommentMessage(SectionController controller, CommentInfo comment)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.AddComment);
+            message.AddString(controller.name);
+            message.AddBinary(comment.toBinary());
+            return message.ToBuffer();
+        }
+
+        public static byte[] RemoveCommentMessage(SectionController controller, CommentInfo comment)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.RemoveComment);
+            message.AddString(controller.name);
+            message.AddUint32((uint)comment.threadID);
+            message.AddUint32((uint)comment.commentID);
+            return message.ToBuffer();
+        }
+
+        public static byte[] UpdateCommentMessage(SectionController controller, CommentInfo comment)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Section);
+            message.AddUint8((byte)SectionMsg.UpdateComment);
+            message.AddString(controller.name);
+            message.AddUint32((uint)comment.commentID);
+            message.AddUint32((uint)comment.threadID);
+            message.AddString(comment.comment);
+            return message.ToBuffer();
+        }
+        
+        public static byte[] ChatMessage(String chat) {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Chat);
+            message.AddUint8((byte)ChatMsg.Msg);
+            message.AddString(chat);
+            return message.ToBuffer();
+        }
+
+        public static byte[] ChatOnlineList(String list)
+        {
+            var message = new MessageWriter();
+            message.AddUint8((byte)Controller.Chat);
+            message.AddUint8((byte)ChatMsg.OnlineList);
+            message.AddString(list);
+            return message.ToBuffer();
+        }
+    }
+}
