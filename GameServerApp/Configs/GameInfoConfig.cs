ff4bd01ab437b1067dcc921206cc8f523fe40e93
Matthew French <matthewf@ksu.edu>
2018-08-27 08:25:16 -0500

    Workgin on json configs

---
+++ GameServerApp/Configs/GameInfoConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace LeagueSandbox.GameServerApp.Logic
+{
+    public class GameInfoConfig
+    {
+        public string ClientLocation { get; private set; } = "C:\\LeagueSandbox\\League_Sandbox_Client";
+        public bool AutoStartClient { get; private set; } = true;
+
+        private GameInfoConfig()
+        {
+        }
+
+        public static GameInfoConfig Default()
+        {
+            return new GameInfoConfig();
+        }
+
+        public static GameInfoConfig LoadFromJson(string json)
+        {
+            var result = new GameInfoConfig();
+            result.LoadConfig(json);
+            return result;
+        }
+
+        public static GameInfoConfig LoadFromFile(string path)
+        {
+            var result = new GameInfoConfig();
+            if (File.Exists(path))
+            {
+                result.LoadConfig(File.ReadAllText(path));
+            }
+            return result;
+        }
+
+        private void LoadConfig(string json)
+        {
+            var data = JObject.Parse(json);
+            AutoStartClient = (bool)data.SelectToken("autoStartClient");
+            ClientLocation = (string)data.SelectToken("clientLocation");
+        }
+    }
+}
---
+++ GameServerApp/Configs/GameServerAppConfig.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LeagueSandbox.GameServerApp.Logic
+{
+    public class GameServerAppConfig
+    {
+        public string ClientLocation { get; private set; } = "C:\\LeagueSandbox\\League_Sandbox_Client";
+        public bool AutoStartClient { get; private set; } = true;
+        public bool AutoStartServer { get; private set; } = true;
+
+        private GameServerAppConfig()
+        {
+        }
+
+        public static GameServerAppConfig Default()
+        {
+            return new GameServerAppConfig();
+        }
+
+        public static GameServerAppConfig LoadFromJson(string json)
+        {
+            var result = new GameServerAppConfig();
+            result.LoadConfig(json);
+            return result;
+        }
+
+        public static GameServerAppConfig LoadFromFile(string path)
+        {
+            var result = new GameServerAppConfig();
+            if (File.Exists(path))
+            {
+                result.LoadConfig(File.ReadAllText(path));
+            }
+            return result;
+        }
+
+        private void LoadConfig(string json)
+        {
+            var data = JObject.Parse(json);
+            AutoStartClient = (bool)data.SelectToken("autoStartClient");
+            AutoStartServer = (bool)data.SelectToken("autoStartServer");
+            ClientLocation = (string)data.SelectToken("clientLocation");
+        }
+
+        private void SaveConfig(string path)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            using (StreamWriter sw = new StreamWriter(path))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, 
+                    new {
+                        clientLocation = ClientLocation,
+                        autoStartClient = AutoStartClient,
+                        autoStartServer = AutoStartServer
+                    }
+                );
+            }
+        }
+    }
+}
