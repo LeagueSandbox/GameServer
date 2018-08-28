13ac41efe8af966099eede24e7f0dad3cdcaa951
MatthewFrench <matthewf@ksu.edu>
2018-08-27 22:20:59 -0500

    Added all properties to JSON

--- GameServerApp/Configs/GameServerAppConfig.cs
+++ GameServerApp/Configs/GameServerAppConfig.cs
@@ -6,9 +6,29 @@ namespace LeagueSandbox.GameServerApp.Logic
 {
     public class GameServerAppConfig
     {
-        public string ClientLocation { get; private set; } = "C:\\LeagueSandbox\\League_Sandbox_Client";
-        public bool AutoStartClient { get; private set; } = true;
-        public bool AutoStartServer { get; private set; } = true;
+        public string GamePath { get; set; } = "C:\\LeagueSandbox\\League_Sandbox_Client";
+        public string GameMode { get; set; } = "LeagueSandbox-Default";
+        public string ContentPath { get; set; } = "../../../..";
+        public string Rank { get; set; } = "Diamond";
+        public string Champion { get; set; } = "Ezreal";
+        public string Name { get; set; } = "Lame";
+        public int Map { get; set; } = 1;
+        public string Team { get; set; } = "Blue";
+        public int Icon { get; set; } = 0;
+        public int Skin { get; set; } = 0;
+        public int Ribbon { get; set; } = 2;
+        public string Summoner1 { get; set; } = "SummonerHeal";
+        public string Summoner2 { get; set; } = "SummonerFlash";
+        public bool EnableCooldowns { get; set; } = false;
+        public bool EnableSpawnMinions { get; set; } = true;
+        public bool EnableManaCosts { get; set; } = false;
+        public bool EnableCheats { get; set; } = true;
+        public bool DisplayDebugLogs { get; set; } = false;
+        public bool DisplayInfoLogs { get; set; } = true;
+        public bool DisplayWarningLogs { get; set; } = true;
+        public bool DisplayErrorLogs { get; set; } = true;
+        public bool AutoStartServerOnLaunch { get; set; } = true;
+        public bool AutoStartGameWithServer { get; set; } = true;
 
         private GameServerAppConfig()
         {
@@ -39,9 +59,30 @@ namespace LeagueSandbox.GameServerApp.Logic
         private void LoadConfig(string json)
         {
             var data = JObject.Parse(json);
-            AutoStartClient = (bool)data.SelectToken("autoStartClient");
-            AutoStartServer = (bool)data.SelectToken("autoStartServer");
-            ClientLocation = (string)data.SelectToken("clientLocation");
+
+            GamePath = (string)data.SelectToken("GamePath");
+            GameMode = (string)data.SelectToken("GameMode");
+            ContentPath = (string)data.SelectToken("ContentPath");
+            Rank = (string)data.SelectToken("Rank");
+            Champion = (string)data.SelectToken("Champion");
+            Name = (string)data.SelectToken("Name");
+            Map = (int)data.SelectToken("Map");
+            Team = (string)data.SelectToken("Team");
+            Icon = (int)data.SelectToken("Icon");
+            Skin = (int)data.SelectToken("Skin");
+            Ribbon = (int)data.SelectToken("Ribbon");
+            Summoner1 = (string)data.SelectToken("Summoner1");
+            Summoner2 = (string)data.SelectToken("Summoner2");
+            EnableCooldowns = (bool)data.SelectToken("EnableCooldowns");
+            EnableSpawnMinions = (bool)data.SelectToken("EnableSpawnMinions");
+            EnableManaCosts = (bool)data.SelectToken("EnableManaCosts");
+            EnableCheats = (bool)data.SelectToken("EnableCheats");
+            DisplayInfoLogs = (bool)data.SelectToken("DisplayInfoLogs");
+            DisplayDebugLogs = (bool)data.SelectToken("DisplayDebugLogs");
+            DisplayWarningLogs = (bool)data.SelectToken("DisplayWarningLogs");
+            DisplayErrorLogs = (bool)data.SelectToken("DisplayErrorLogs");
+            AutoStartServerOnLaunch = (bool)data.SelectToken("AutoStartServerOnLaunch");
+            AutoStartGameWithServer = (bool)data.SelectToken("AutoStartGameWithServer");
         }
 
         private void SaveConfig(string path)
@@ -52,11 +93,32 @@ namespace LeagueSandbox.GameServerApp.Logic
             using (StreamWriter sw = new StreamWriter(path))
             using (JsonWriter writer = new JsonTextWriter(sw))
             {
-                serializer.Serialize(writer, 
-                    new {
-                        clientLocation = ClientLocation,
-                        autoStartClient = AutoStartClient,
-                        autoStartServer = AutoStartServer
+                serializer.Serialize(writer,
+                    new
+                    {
+                        GamePath,
+                        GameMode,
+                        ContentPath,
+                        Rank,
+                        Champion,
+                        Name,
+                        Map,
+                        Team,
+                        Icon,
+                        Skin,
+                        Ribbon,
+                        Summoner1,
+                        Summoner2,
+                        EnableCooldowns,
+                        EnableSpawnMinions,
+                        EnableManaCosts,
+                        EnableCheats,
+                        DisplayInfoLogs,
+                        DisplayDebugLogs,
+                        DisplayWarningLogs,
+                        DisplayErrorLogs,
+                        AutoStartServerOnLaunch,
+                        AutoStartGameWithServer
                     }
                 );
             }
