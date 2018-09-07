73bdd20d5c7414a571996299f14a8ccf62bbcece
Lukas Wirth <lukastw97@gmail.com>
2018-09-07 22:07:10 +0200

    Remove forgotten comment

--- GameServerCore/Domain/IItem.cs
+++ GameServerCore/Domain/IItem.cs
@@ -3,7 +3,6 @@
     public interface IItem
     {
         byte StackSize { get; }
-        // todo: Unused atm, is this needed?
         int TotalPrice { get; }
         IItemType ItemType { get; }
     }
