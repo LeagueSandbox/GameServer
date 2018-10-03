9521bd36f761a674fa25df33668f79893fbb6bf7
zaksh <zohaib.sharani@gmail.com>
2018-10-03 18:13:03 +0500

    Use 'using static' for ApiFunctionManager function calls
    Solves https://github.com/LeagueSandbox/GameServer/issues/739

--- GameServerLibTests/Tests/API/ApiFunctionManagerTests.cs
+++ GameServerLibTests/Tests/API/ApiFunctionManagerTests.cs
@@ -1,4 +1,5 @@
 ﻿using LeagueSandbox.GameServer.API;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LeagueSandbox.GameServerTests.Tests.API
@@ -10,22 +11,22 @@ namespace LeagueSandbox.GameServerTests.Tests.API
         public void StringToByteArrayTest()
         {
             byte[] shouldBe = { 0xC6, 0x00, 0x10, 0x45, 0xF8};
-            var byteArray = ApiFunctionManager.StringToByteArray("C6001045F8");
+            var byteArray = StringToByteArray("C6001045F8");
             CollectionAssert.AreEqual(shouldBe, byteArray);
 
-            byteArray = ApiFunctionManager.StringToByteArray("C6 00 10 45 F8");
+            byteArray = StringToByteArray("C6 00 10 45 F8");
             CollectionAssert.AreEqual(shouldBe, byteArray);
 
-            byteArray = ApiFunctionManager.StringToByteArray("C60010       45F8");
+            byteArray = StringToByteArray("C60010       45F8");
             CollectionAssert.AreEqual(shouldBe, byteArray);
 
-            byteArray = ApiFunctionManager.StringToByteArray("C6001045F8      ");
+            byteArray = StringToByteArray("C6001045F8      ");
             CollectionAssert.AreEqual(shouldBe, byteArray);
 
-            byteArray = ApiFunctionManager.StringToByteArray("    C6001045F8");
+            byteArray = StringToByteArray("    C6001045F8");
             CollectionAssert.AreEqual(shouldBe, byteArray);
 
-            byteArray = ApiFunctionManager.StringToByteArray("C600   1045  F8");
+            byteArray = StringToByteArray("C600   1045  F8");
             CollectionAssert.AreEqual(shouldBe, byteArray);
         }
     }
