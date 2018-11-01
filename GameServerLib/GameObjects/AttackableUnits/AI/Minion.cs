f5add0166b823d3f6f572bc28d17a807e2414e54
ICsleep <XdTube@yahoo.de>
2018-11-01 11:56:32 +0100

    Fix Minion Movment
    Stops Minions from moving awkward after losing all targets.

--- GameServerLib/GameObjects/AttackableUnits/AI/Minion.cs
+++ GameServerLib/GameObjects/AttackableUnits/AI/Minion.cs
@@ -146,6 +146,9 @@ namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI
                 return true;
             }
 
+            _game.PacketNotifier.NotifyStopAutoAttack(this);
+            IsAttacking = false;
+
             return false;
         }
 
