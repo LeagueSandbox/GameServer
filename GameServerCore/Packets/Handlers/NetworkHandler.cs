305164840d662783c5aae2f31d76ca009bfc3812
danil179 <32551661+danil179@users.noreply.github.com>
2018-11-20 00:21:23 +0200

    Progress #696 -- Implement generic NetworkHandler and new types
    * Added generic handler and new types.
    
    Progress #696

---
+++ GameServerCore/Packets/Handlers/NetworkHandler.cs
@@ -0,0 +1,33 @@
+﻿using GameServerCore.Packets.PacketDefinitions;
+using System;
+using System.Collections.Generic;
+
+namespace GameServerCore.Packets
+{
+    // the global generic network handler between the bridge and the server
+    class NetworkHandler
+    {
+        public delegate void MessageHandler<T>(T msg) where T : ICoreMessage;
+        private readonly Dictionary<Type, List<Delegate>> _handlers = new Dictionary<Type, List<Delegate>>();
+
+        // server & scripts will register to events, this allows scripts more than the current state
+        public void Register<T>(MessageHandler<T> handler) where T: ICoreMessage
+        {
+            if (handler == null) return;
+            _handlers[typeof(T)].Add(handler);
+        }
+        // every message (bridge->server or server->bridge) pass should pass here
+        public void OnMessage<T>(T req) where T: ICoreMessage
+        {
+            Handle<T>(req);
+        }
+        private void Handle<T>(T message) where T: ICoreMessage
+        {
+            List<MessageHandler<T>> handlerList = _handlers[typeof(T)] as List<MessageHandler<T>>;
+            foreach(MessageHandler<T> handler in handlerList)
+            {
+                handler(message);
+            }
+        }
+    }
+}
---
+++ GameServerCore/Packets/PacketDefinitions/IPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServerCore.Packets.PacketDefinitions
+{
+    // all packets in PacketDefinitions should implement this
+    public interface IPacket
+    {
+        // for requests
+        ICoreMessage Read(byte[] data);
+        // for responses
+        void Write(ICoreMessage msg);
+    }
+}
