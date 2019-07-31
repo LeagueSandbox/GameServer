f11654cbfbea4a8ea11f3153f1ab79ff7347dfb0
danil179 <32551661+danil179@users.noreply.github.com>
2019-07-31 17:41:26 +0300

    Resolve #853 -- Add A* algo (#848)
    * Implementing A* algo instead of using lib

--- GameServerCore/Content/INavGrid.cs
+++ GameServerCore/Content/INavGrid.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 using GameServerCore.Domain.GameObjects;
 
 namespace GameServerCore.Content
@@ -28,6 +29,7 @@ namespace GameServerCore.Content
         float GetHeightAtLocation(float x, float y);
         bool IsWalkable(Vector2 coords);
         bool IsWalkable(float x, float y);
+        List<Vector2> GetPath(Vector2 start, Vector2 end);
         Vector2 GetSize();
         bool IsAnythingBetween(IGameObject a, IGameObject b);
         Vector2 GetClosestTerrainExit(Vector2 location);
