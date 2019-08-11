2cf489706fe349c016214222aede8632836d89b1
danil179 <32551661+danil179@users.noreply.github.com>
2019-08-11 14:31:26 +0300

    Add SyncId

--- PacketDefinitions420/PacketNotifier.cs
+++ PacketDefinitions420/PacketNotifier.cs
@@ -721,7 +721,7 @@ namespace PacketDefinitions420
             var md = new MovementDataStop();
             md.Position = minion.GetPosition();
             md.Forward = new Vector2(0, 1);
-            md.SyncID = 0x0006E4CF; //TODO: generate real movement SyncId
+            md.SyncID = (int)minion.SyncId;
             visionPacket.MovementData = md;
             visionPacket.Packets.Add(spawnPacket);
             visionPacket.SenderNetID = minion.NetId;
@@ -781,7 +781,7 @@ namespace PacketDefinitions420
             {
                 Position = u.GetPosition(),
                 Forward = new Vector2(0, 1),
-                SyncID = 0x0006E4CF //TODO: generate real movement SyncId
+                SyncID = (int)u.SyncId
             };
             enterVis.MovementData = md;
             enterVis.SenderNetID = u.NetId;
