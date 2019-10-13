45d0795f8702c9b5712e3ad925972aefbd9fe7bf
danil179 <32551661+danil179@users.noreply.github.com>
2019-10-13 05:37:13 +0300

    Quadtree implemented
    Fix Y axis
    
    Fixing bug cause of too strict values
    
    Fixing updating dynamic objects
    
    Removing config files

--- GameServerLib/GameObjects/Other/CollisionHandler.cs
+++ GameServerLib/GameObjects/Other/CollisionHandler.cs
@@ -5,14 +5,17 @@ using GameServerCore.Maps;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings.AnimatedBuildings;
 using LeagueSandbox.GameServer.Maps;
+using UltimateQuadTree;
 
 namespace LeagueSandbox.GameServer.GameObjects.Other
 {
     public class CollisionHandler: ICollisionHandler
     {
-        private Game _game;
+        private Game _game; //TODO: get rid of this thing YAGNI!
 
         private readonly List<IGameObject> _objects = new List<IGameObject>();
+        // This is the 'static map'
+        private readonly QuadTree<IGameObject> _quadMap = new QuadTree<IGameObject>(20000,20000,new CollisonObject()); //TODO: initialize the proper values of width and height for every map
 
         public CollisionHandler(Game game, IMap map)
         {
@@ -24,17 +27,29 @@ namespace LeagueSandbox.GameServer.GameObjects.Other
         public void AddObject(IGameObject obj)
         {
             _objects.Add(obj);
+            if (obj is IBaseTurret || obj is IInhibitor || obj is INexus)
+            {
+                _quadMap.Insert(obj);
+            }
+            
         }
 
         public void RemoveObject(IGameObject obj)
         {
             _objects.Remove(obj);
+            if (obj is IBaseTurret || obj is IInhibitor || obj is INexus)
+            {
+                _quadMap.Remove(obj);
+            }
         }
 
         public void Update()
         {
+            QuadTree<IGameObject> _quadDynamic = new QuadTree<IGameObject>(20000, 20000, new CollisonObject());
+            _quadDynamic.InsertRange(_objects);
             foreach (var obj in _objects)
             {
+                // static objects, skip collison as they are static
                 if (obj is IBaseTurret || obj is IInhibitor || obj is INexus)
                 {
                     continue;
@@ -42,16 +57,11 @@ namespace LeagueSandbox.GameServer.GameObjects.Other
 
                 if (!_game.Map.NavGrid.IsWalkable(obj.X, obj.Y))
                 {
-                    obj.OnCollision(null);
+                    obj.OnCollision(null); // TODO: Change this to something more readable like `bool terrain`
                 }
 
-                foreach (var obj2 in _objects)
+                foreach (var obj2 in _quadDynamic.GetNearestObjects(obj))
                 {
-                    if (obj == obj2)
-                    {
-                        continue;
-                    }
-
                     if (obj.IsCollidingWith(obj2))
                     {
                         obj.OnCollision(obj2);
---
+++ GameServerLib/GameObjects/Other/CollisonObject.cs
@@ -0,0 +1,34 @@
+﻿using GameServerCore.Domain.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateQuadTree;
+
+namespace LeagueSandbox.GameServer.GameObjects.Other
+{
+    public class CollisonObject : IQuadTreeObjectBounds<IGameObject>
+    {
+        public double GetLeft(IGameObject obj)
+        {
+            return obj.X - obj.CollisionRadius -1;
+        }
+
+        public double GetRight(IGameObject obj)
+        {
+            return obj.X + obj.CollisionRadius +1;
+        }
+
+        public double GetTop(IGameObject obj)
+        {
+            return (20000 - obj.Y) - obj.CollisionRadius -1; //TODO: check if this is the right direction of Y
+        }
+
+        public double GetBottom(IGameObject obj)
+        {
+            return (20000 - obj.Y) + obj.CollisionRadius +1;
+        }
+    }
+
+}
