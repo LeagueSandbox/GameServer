f3350e73a234a43fa6272e52be89fe83567297f4
xxx3456789 <46378803+xxx3456789@users.noreply.github.com>
2019-12-08 18:40:35 +0800

    kind of fix, make it looks normal in game

--- Content/LeagueSandbox-Scripts/Buffs/Yasuo/YasuoE.cs
+++ Content/LeagueSandbox-Scripts/Buffs/Yasuo/YasuoE.cs
@@ -1,7 +1,8 @@
-﻿using GameServerCore.Domain;
+using GameServerCore.Domain;
 using GameServerCore.Domain.GameObjects;
 using GameServerCore.Enums;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.Stats;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using System.Numerics;
 
@@ -13,15 +14,13 @@ namespace YasuoE
 
         public void OnActivate(IObjAiBase unit, ISpell ownerSpell)
         {
-            var test = ownerSpell.SpellData.LocationTargettingWidth[0];
             var time = 0.6f - ownerSpell.Level * 0.1f;
             var damage = 50f + ownerSpell.Level * 20f + unit.Stats.AbilityPower.Total * 0.6f;
-            var to = Vector2.Normalize(target.GetPosition() - unit.GetPosition());
             AddParticleTarget((IChampion)unit, "Yasuo_Base_E_Dash.troy", unit);
             AddParticleTarget((IChampion)unit, "Yasuo_Base_E_dash_hit.troy", target);
             AddBuffHudVisual("YasuoDash", time, 1, BuffType.COMBAT_DEHANCER, unit, time);
-
-            DashToLocation(unit, target.X + to.X * test, target.Y + to.Y * test, 750f + unit.Stats.MoveSpeed.Total * 0.6f, false, "SPELL3");
+            var to = Vector2.Normalize(target.GetPosition() - unit.GetPosition());
+            DashToLocation(unit, target.X + to.X * 175f, target.Y + to.Y * 175f, 750f + unit.Stats.MoveSpeed.Total * 0.6f, false, "SPELL3");
             target.TakeDamage(unit, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
         }
 
