LeagueSandbox/GameServer

commit 06a4039ee24228e457c142af173294bf12c7a5da

Resolve #580 -- Temporary fix for attacking inhibs/nexus (#845)
This fix using collison radius calculations in order to add some radius for the buildings. That fix the problems with attacking inhibs and the nexus.

Resolve #580

GameServerLib/Content/NavGrid.cs
@@ -542,7 +542,16 @@ namespace LeagueSandbox.GameServer.Content
 
         public bool IsAnythingBetween(IGameObject a, IGameObject b)
         {
-            return CastRaySqr(a.GetPosition(), b.GetPosition()) < (b.GetPosition() - a.GetPosition()).SqrLength();
+            var rayDist = Math.Sqrt(CastRaySqr(a.GetPosition(), b.GetPosition()));
+            if (a is IObjBuilding)
+            {
+                rayDist += a.CollisionRadius;
+            }
+            if (b is IObjBuilding)
+            {
+                rayDist += b.CollisionRadius;
+            }
+            return rayDist*rayDist <= (b.GetPosition() - a.GetPosition()).SqrLength();
         }
 
         public Vector2 GetClosestTerrainExit(Vector2 location)

GameServerLib/Maps/SummonersRift.cs
@@ -278,7 +278,7 @@ namespace LeagueSandbox.GameServer.Maps
             _game.ObjectManager.AddObject(new LevelProp(_game, -99.5613f, 855.6632f, 191.4039f, 158.0f, 0.0f, 0.0f, 0.0f, 0.0f, "LevelProp_ShopMale1", "ShopMale"));
 
             //TODO
-            var collisionRadius = 0;
+            var collisionRadius = 400;
             var sightRange = 1700;
 
             _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, collisionRadius, 853.368f, 3345.1645f, sightRange, 0xffd23c3e)); //top exact

GameServerLib/GameObjects/AttackableUnits/AttackableUnit.cs
@@ -44,7 +44,8 @@ namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits
             Logger = LoggerProvider.GetLogger();
             Stats = stats;
             Model = model;
-            CollisionRadius = 40;
+            if (collisionRadius != 0) CollisionRadius = collisionRadius;
+            else CollisionRadius = 40;
             Stats.AttackSpeedMultiplier.BaseValue = 1.0f;
         }
 

commit 98c89445550f33d0e32237f6c0f582cccb943115

Fix Bugs Introduced by #845 and QuadTree
1. Fixes vision issues
2. Fixes collision issues, specifically ones regarding buildings

GameServerLib/Content/NavGrid.cs
@@ -544,7 +544,7 @@ namespace LeagueSandbox.GameServer.Content
 
         public bool IsAnythingBetween(IGameObject a, IGameObject b)
         {
-            var rayDist = Math.Sqrt(CastRaySqr(a.GetPosition(), b.GetPosition()));
+            var rayDist = CastRaySqr(a.GetPosition(), b.GetPosition());
             if (a is IObjBuilding)
             {
                 rayDist += a.CollisionRadius;
@@ -553,7 +553,7 @@ namespace LeagueSandbox.GameServer.Content
             {
                 rayDist += b.CollisionRadius;
             }
-            return rayDist*rayDist <= (b.GetPosition() - a.GetPosition()).SqrLength();
+            return rayDist < (b.GetPosition() - a.GetPosition()).SqrLength();
         }
 
         public Vector2 GetClosestTerrainExit(Vector2 location)

GameServerLib/GameObjects/Other/CollisionHandler.cs
@@ -15,7 +15,7 @@ namespace LeagueSandbox.GameServer.GameObjects.Other
 
         private readonly List<IGameObject> _objects = new List<IGameObject>();
         // This is the 'static map'
-        private readonly QuadTree<IGameObject> _quadMap = new QuadTree<IGameObject>(20000,20000,new CollisonObject()); //TODO: initialize the proper values of width and height for every map
+        private readonly QuadTree<IGameObject> _quadMap = new QuadTree<IGameObject>(16000, 16000, new CollisionObject()); //TODO: initialize the proper values of width and height for every map
 
         public CollisionHandler(Game game, IMap map)
         {
@@ -45,7 +45,7 @@ namespace LeagueSandbox.GameServer.GameObjects.Other
 
         public void Update()
         {
-            QuadTree<IGameObject> _quadDynamic = new QuadTree<IGameObject>(20000, 20000, new CollisonObject());
+            QuadTree<IGameObject> _quadDynamic = new QuadTree<IGameObject>(16000, 16000, new CollisionObject());
             _quadDynamic.InsertRange(_objects);
             foreach (var obj in _objects)
             {

GameServerLib/GameObjects/Other/CollisionObject.cs
@@ -8,26 +8,26 @@ using UltimateQuadTree;
 
 namespace LeagueSandbox.GameServer.GameObjects.Other
 {
-    public class CollisonObject : IQuadTreeObjectBounds<IGameObject>
+    public class CollisionObject : IQuadTreeObjectBounds<IGameObject>
     {
         public double GetLeft(IGameObject obj)
         {
-            return obj.X - obj.CollisionRadius -1;
+            return obj.X - obj.CollisionRadius - 1;
         }
 
         public double GetRight(IGameObject obj)
         {
-            return obj.X + obj.CollisionRadius +1;
+            return obj.X + obj.CollisionRadius + 1;
         }
 
         public double GetTop(IGameObject obj)
         {
-            return (20000 - obj.Y) - obj.CollisionRadius -1; //TODO: check if this is the right direction of Y
+            return obj.Y + obj.CollisionRadius + 1;
         }
 
         public double GetBottom(IGameObject obj)
         {
-            return (20000 - obj.Y) + obj.CollisionRadius +1;
+            return obj.Y - obj.CollisionRadius - 1;
         }
     }
 

GameServerLib/Maps/SummonersRift.cs
@@ -241,9 +241,9 @@ namespace LeagueSandbox.GameServer.Maps
                 TurretType.INNER_TURRET, GetTurretItems(TurretType.INNER_TURRET)));
             _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T1_C_03_A", 3233.99f, 3447.24f, TeamId.TEAM_BLUE,
                 TurretType.INHIBITOR_TURRET, GetTurretItems(TurretType.INHIBITOR_TURRET)));
-            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T1_C_01_A", 1341.63f, 2029.98f, TeamId.TEAM_BLUE,
+            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T1_C_01_A", 1271.097f, 1989.8077f, TeamId.TEAM_BLUE,
                 TurretType.NEXUS_TURRET, GetTurretItems(TurretType.NEXUS_TURRET)));
-            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T1_C_02_A", 1768.19f, 1589.47f, TeamId.TEAM_BLUE,
+            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T1_C_02_A", 1821.097f, 1589.8077f, TeamId.TEAM_BLUE,
                 TurretType.NEXUS_TURRET, GetTurretItems(TurretType.NEXUS_TURRET)));
             _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T2_C_05_A", 8548.0f, 8289.0f, TeamId.TEAM_PURPLE,
                 TurretType.OUTER_TURRET, GetTurretItems(TurretType.OUTER_TURRET)));
@@ -251,9 +251,9 @@ namespace LeagueSandbox.GameServer.Maps
                 TurretType.INNER_TURRET, GetTurretItems(TurretType.INNER_TURRET)));
             _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T2_C_03_A", 10743.0f, 11010.0f, TeamId.TEAM_PURPLE,
                 TurretType.INHIBITOR_TURRET, GetTurretItems(TurretType.INHIBITOR_TURRET)));
-            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T2_C_01_A", 12662.0f, 12442.0f, TeamId.TEAM_PURPLE,
-                TurretType.NEXUS_TURRET, GetTurretItems(TurretType.NEXUS_TURRET)));
-            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T2_C_02_A", 12118.0f, 12876.0f, TeamId.TEAM_PURPLE,
+            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T2_C_01_A", 12621.097f, 12364.8077f, TeamId.TEAM_PURPLE,
+               TurretType.NEXUS_TURRET, GetTurretItems(TurretType.NEXUS_TURRET)));
+            _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_T2_C_02_A", 12171.097f, 12789.8077f, TeamId.TEAM_PURPLE,
                 TurretType.NEXUS_TURRET, GetTurretItems(TurretType.NEXUS_TURRET)));
             _game.ObjectManager.AddObject(new LaneTurret(_game, "Turret_OrderTurretShrine_A", -236.05f, -53.32f, TeamId.TEAM_BLUE,
                 TurretType.FOUNTAIN_TURRET, GetTurretItems(TurretType.FOUNTAIN_TURRET)));
@@ -277,18 +277,19 @@ namespace LeagueSandbox.GameServer.Maps
             _game.ObjectManager.AddObject(new LevelProp(_game, 13374.17f, 14245.673f, 194.9741f, 224.0f, 33.33f, 0.0f, 0.0f, -44.44f, "LevelProp_ShopMale", "ShopMale"));
             _game.ObjectManager.AddObject(new LevelProp(_game, -99.5613f, 855.6632f, 191.4039f, 158.0f, 0.0f, 0.0f, 0.0f, 0.0f, "LevelProp_ShopMale1", "ShopMale"));
 
-            //TODO
-            var collisionRadius = 400;
+            //TODO: Unhardcode everything (preferably by reading from content)
+            var inhibRadius = 350;
+            var nexusRadius = 550;
             var sightRange = 1700;
 
-            _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, collisionRadius, 853.368f, 3345.1645f, sightRange, 0xffd23c3e)); //top exact
-            _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, collisionRadius, 2785, 3000, sightRange, 0xff4a20f1)); //mid exact
-            _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, collisionRadius, 3044, 1070, sightRange, 0xff9303e1)); //bot exact
-            _game.ObjectManager.AddObject(new Inhibitor(_game, "ChaosInhibitor", TeamId.TEAM_PURPLE, collisionRadius, 10962.21f, 13422.915f, sightRange, 0xff6793d0)); //top exact          
-            _game.ObjectManager.AddObject(new Inhibitor(_game, "ChaosInhibitor", TeamId.TEAM_PURPLE, collisionRadius, 11225.62402f, 11425.87012f, sightRange, 0xffff8f1f)); //mid exact            
-            _game.ObjectManager.AddObject(new Inhibitor(_game, "ChaosInhibitor", TeamId.TEAM_PURPLE, collisionRadius, 13200, 11200, sightRange, 0xff26ac0f)); //bot exact
-            _game.ObjectManager.AddObject(new Nexus(_game, "OrderNexus", TeamId.TEAM_BLUE, collisionRadius, 1170, 1470, sightRange, 0xfff97db5));
-            _game.ObjectManager.AddObject(new Nexus(_game, "ChaosNexus", TeamId.TEAM_PURPLE, collisionRadius, 12804.69f, 13041.465f, sightRange, 0xfff02c0f));
+            _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, inhibRadius, 796.097f, 3339.8077f, sightRange, 0xffd23c3e)); //top
+            _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, inhibRadius, 2746.097f, 2964.8077f, sightRange, 0xff4a20f1)); //mid
+            _game.ObjectManager.AddObject(new Inhibitor(_game, "OrderInhibitor", TeamId.TEAM_BLUE, inhibRadius, 2996.097f, 1014.8077f, sightRange, 0xff9303e1)); //bot
+            _game.ObjectManager.AddObject(new Inhibitor(_game, "ChaosInhibitor", TeamId.TEAM_PURPLE, inhibRadius, 10946.097f, 13414.8077f, sightRange, 0xff6793d0)); //top
+            _game.ObjectManager.AddObject(new Inhibitor(_game, "ChaosInhibitor", TeamId.TEAM_PURPLE, inhibRadius, 11196.097f, 11439.8077f, sightRange, 0xffff8f1f)); //mid
+            _game.ObjectManager.AddObject(new Inhibitor(_game, "ChaosInhibitor", TeamId.TEAM_PURPLE, inhibRadius, 13196.097f, 11164.8077f, sightRange, 0xff26ac0f)); //bot
+            _game.ObjectManager.AddObject(new Nexus(_game, "OrderNexus", TeamId.TEAM_BLUE, nexusRadius, 1146.097f, 1414.8077f, sightRange, 0xfff97db5));
+            _game.ObjectManager.AddObject(new Nexus(_game, "ChaosNexus", TeamId.TEAM_PURPLE, nexusRadius, 12771.097f, 13014.8077f, sightRange, 0xfff02c0f));
         }
 
         public void Update(float diff)