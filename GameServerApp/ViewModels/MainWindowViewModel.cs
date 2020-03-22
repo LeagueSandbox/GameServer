55f20377711697c80c52630f2b0d12903a4b7d2c
Matthew French <matthewf@ksu.edu>
2020-03-21 23:32:31 -0500

    Added GameServerApp Avalonia project

---
+++ GameServerApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServerApp.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to GameServer!";
+    }
+}
