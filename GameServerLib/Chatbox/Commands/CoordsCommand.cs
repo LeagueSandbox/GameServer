37803f6362a9055b2020d497dee25a558f200980
Kubeer <47148920+Kubeer@users.noreply.github.com>
2020-08-27 20:42:31 +0200

    Resolve #423 -- Added direction data in coords command (#983)
    * Added direction and rotation data of the player when moving in coords command.
    * Changed angle 0 axis and added description

--- GameServerLib/Chatbox/Commands/CoordsCommand.cs
+++ GameServerLib/Chatbox/Commands/CoordsCommand.cs
@@ -1,6 +1,8 @@
 ﻿using GameServerCore;
 using LeagueSandbox.GameServer.Logging;
 using log4net;
+using System.Numerics;
+using System;
 
 namespace LeagueSandbox.GameServer.Chatbox.Commands
 {
@@ -23,7 +25,15 @@ namespace LeagueSandbox.GameServer.Chatbox.Commands
         {
             var champion = _playerManager.GetPeerInfo((ulong)userId).Champion;
             _logger.Debug($"At {champion.X}; {champion.Y}");
-            var msg = $"At Coords - X: {champion.X} Y: {champion.Y} Z: {champion.GetZ()}";
+            var dirMsg = "Not moving anywhere";
+            if (!champion.IsPathEnded())
+            {
+                Vector2 dir = champion.GetDirection();
+                // Angle measured between [1, 0] and player direction vectors (to X axis)
+                double ang = Math.Acos(dir.X / dir.Length()) * (180 / Math.PI); 
+                dirMsg = $"dirX: {dir.X} dirY: {dir.Y} dirAngle (to X axis): {ang}";
+            }
+            var msg = $"At Coords - X: {champion.X} Y: {champion.Y} Z: {champion.GetZ()} "+dirMsg;
             ChatCommandManager.SendDebugMsgFormatted(DebugMsgType.NORMAL, msg);
         }
     }
