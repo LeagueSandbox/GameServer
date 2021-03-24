9b930233d3ec861166bd40b1e8de287c6485258a
microsoftv <reptilian532@hotmail.com>
2021-03-23 21:53:01 -0400

    Fix Nasus BasicAttack and Remove Unused NasusQ

--- Content/LeagueSandbox-Scripts/Champions/Nasus/BasicAttack.cs
+++ Content/LeagueSandbox-Scripts/Champions/Nasus/BasicAttack.cs
@@ -13,7 +13,6 @@ namespace Spells
     {
         public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
-            TriggersSpellCasts = true
             // TODO
         };
 
@@ -27,6 +26,12 @@ namespace Spells
 
         public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
+            ApiEventManager.OnLaunchAttack.AddListener(this, owner, OnLaunchAttack, false);
+        }
+
+        public void OnLaunchAttack(ISpell spell)
+        {
+            spell.CastInfo.Owner.SetAutoAttackSpell("NasusBasicAttack2", false);
         }
 
         public void OnSpellCast(ISpell spell)
@@ -35,7 +40,6 @@ namespace Spells
 
         public void OnSpellPostCast(ISpell spell)
         {
-            spell.CastInfo.Owner.SetAutoAttackSpell("NasusBasicAttack2", false);
         }
 
         public void OnUpdate(float diff)
@@ -47,7 +51,6 @@ namespace Spells
     {
         public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
-            TriggersSpellCasts = true
             // TODO
         };
 
@@ -61,57 +64,20 @@ namespace Spells
 
         public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
         {
+            ApiEventManager.OnLaunchAttack.AddListener(this, owner, OnLaunchAttack, false);
         }
 
-        public void OnSpellCast(ISpell spell)
-        {
-        }
-
-        public void OnSpellPostCast(ISpell spell)
+        public void OnLaunchAttack(ISpell spell)
         {
             spell.CastInfo.Owner.SetAutoAttackSpell("NasusBasicAttack", false);
         }
 
-        public void OnUpdate(float diff)
-        {
-        }
-    }
-
-    public class NasusQAttack : ISpellScript
-    {
-        public ISpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
-        {
-            // TODO
-        };
-
-        public void OnActivate(IObjAiBase owner, ISpell spell)
-        {
-            ApiEventManager.OnSpellHit.AddListener(this, new KeyValuePair<ISpell, IObjAiBase>(spell, owner), ApplyEffects, false);
-        }
-
-        public void OnDeactivate(IObjAiBase owner, ISpell spell)
-        {
-        }
-
-        public void OnSpellPreCast(IObjAiBase owner, ISpell spell, IAttackableUnit target, Vector2 start, Vector2 end)
-        {
-        }
-
         public void OnSpellCast(ISpell spell)
         {
         }
 
         public void OnSpellPostCast(ISpell spell)
         {
-        }
-
-        public void ApplyEffects(ISpell spell, IAttackableUnit target, IProjectile projectile)
-        {
-            //RemoveParticleSilent(pTar);
-            target.TakeDamage(spell.CastInfo.Owner, 30f + (20f * (spell.CastInfo.SpellLevel - 1)) + spell.CastInfo.Owner.GetBuffWithName("NasusQStacks").StackCount, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_ATTACK, spell.CastInfo.Owner.IsNextAutoCrit);
-            spell.CastInfo.Owner.GetBuffWithName("NasusQ").DeactivateBuff();
-
-            ApiEventManager.OnSpellHit.RemoveListener(this, new KeyValuePair<ISpell, IObjAiBase>(spell, spell.CastInfo.Owner));
         }
 
         public void OnUpdate(float diff)
