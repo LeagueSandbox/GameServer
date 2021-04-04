5e2c77f41ad27836fa5106c6f668bc697c089d48
microsoftv <reptilian532@hotmail.com>
2021-04-04 16:16:20 -0400

    Fix SetSpell for Uninitialized Spell Slots

--- GameServerLib/GameObjects/AttackableUnits/AI/ObjAIBase.cs
+++ GameServerLib/GameObjects/AttackableUnits/AI/ObjAIBase.cs
@@ -771,16 +771,14 @@ namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI
         /// <returns>Newly created spell set.</returns>
         public ISpell SetSpell(string name, byte slot, bool enabled)
         {
-            if (Spells[slot].CastInfo.IsAutoAttack)
+            if (!Spells.ContainsKey(slot) || Spells[slot].CastInfo.IsAutoAttack)
             {
                 return null;
             }
+
             ISpell newSpell = new Spell.Spell(_game, this, name, slot);
 
-            if (Spells[slot] != null)
-            {
-                newSpell.SetLevel(Spells[slot].CastInfo.SpellLevel);
-            }
+            newSpell.SetLevel(Spells[slot].CastInfo.SpellLevel);
 
             Spells[slot] = newSpell;
             Stats.SetSpellEnabled(slot, enabled);
