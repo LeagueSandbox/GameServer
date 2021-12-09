4ad8a499032df7d9681133e587232fbb4c77fe86
Lizardy <6063922+microsoftv@users.noreply.github.com>
2021-12-08 21:52:10 -0500

    Update IMapScriptMetadata.cs

--- GameServerCore/Scripting/CSharp/IMapScriptMetadata.cs
+++ GameServerCore/Scripting/CSharp/IMapScriptMetadata.cs
@@ -7,7 +7,7 @@ namespace GameServerCore.Scripting.CSharp
     {
         bool EnableBuildingProtection { get; set; }
         /// <summary>
-        /// Wether or not minions are enabled (This can cause issues if enabled and all structures aren't perfectly setup)
+        /// Whether or not minions are enabled (This can cause issues if enabled and all structures aren't perfectly setup)
         /// </summary>
         bool MinionSpawnEnabled { get; set; }
         /// <summary>
@@ -15,7 +15,7 @@ namespace GameServerCore.Scripting.CSharp
         /// </summary>
         long SpawnInterval { get; set; }
         /// <summary>
-        /// Wether the map should use hardcoded minion path coordinates or use the one from league's files (if available) (Default: false)
+        /// Whether the map should use hardcoded minion path coordinates or use the one from league's files (if available) (Default: false)
         /// </summary>
         bool MinionPathingOverride { get; set; }
         /// <summary>
@@ -27,11 +27,11 @@ namespace GameServerCore.Scripting.CSharp
         /// </summary>
         float StartingGold { get; set; }
         /// <summary>
-        /// Wether or not players should be rewarded a decreasing ammount of gold based on how many times they died since their last kill (Default: true)
+        /// Whether or not players should be rewarded a decreasing ammount of gold based on how many times they died since their last kill (Default: true)
         /// </summary>
         bool IsKillGoldRewardReductionActive { get; set; }
         /// <summary>
-        /// What Item should be used as the "Recall Spell", default recall spell is "bluepill", with ID: 2001 (Deafult: 2001)
+        /// The ItemID that should be used as the "Recall Spell", default recall spell is "BluePill", with ID: 2001 (Default: 2001)
         /// </summary>
         int RecallSpellItemId { get; set; }
         /// <summary>
@@ -39,7 +39,7 @@ namespace GameServerCore.Scripting.CSharp
         /// </summary>
         long FirstGoldTime { get; set; }
         /// <summary>
-        /// Wether or not the fountain should heal the players (Default = true)
+        /// Whether or not the fountain should heal the players (Default = true)
         /// </summary>
         bool EnableFountainHealing { get; set; }
     }
