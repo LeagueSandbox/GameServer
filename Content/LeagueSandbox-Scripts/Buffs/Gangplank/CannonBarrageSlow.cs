9d1a2350632de69e701487ce0b3367a43b86e4e1
Lonick32 <39029077+Lonick32@users.noreply.github.com>
2022-03-16 20:13:22 +0200

    Create CannonBarrageSlow.cs

---
+++ Content/LeagueSandbox-Scripts/Buffs/Gangplank/CannonBarrageSlow.cs
@@ -0,0 +1,35 @@
+using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.Stats;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+
+namespace Buffs
+{
+    internal class CannonBarrageSlow : IBuffGameScript
+    {
+        public IBuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.COMBAT_DEHANCER,
+            BuffAddType = BuffAddType.REPLACE_EXISTING,
+            MaxStacks = 1
+        };
+
+        public IStatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        public void OnActivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+            StatsModifier.MoveSpeed.PercentBonus = -0.25f;
+            unit.AddStatModifier(StatsModifier);
+        }
+
+        public void OnDeactivate(IAttackableUnit unit, IBuff buff, ISpell ownerSpell)
+        {
+        }
+
+        public void OnUpdate(float diff)
+        {
+        }
+    }
+}
