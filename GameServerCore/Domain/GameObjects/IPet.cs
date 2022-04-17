bcbd7b51ce93d451eb4750d32916e148bccee8d3
cabeca143 <72713387+cabeca1143@users.noreply.github.com>
2022-04-16 22:17:16 -0300

    Pet init

---
+++ GameServerCore/Domain/GameObjects/IPet.cs
@@ -0,0 +1,11 @@
+﻿namespace GameServerCore.Domain.GameObjects
+{
+    public interface IPet : IMinion
+    {
+        public IBuff CloneBuff { get; }
+        public float LifeTime { get; }
+        public bool CloneInventory { get; }
+        public bool DoFade { get; }
+        public bool ShowMinimapIfClone { get; }
+    }
+}
---
+++ GameServerLib/GameObjects/AttackableUnits/AI/Pet.cs
@@ -0,0 +1,50 @@
+﻿using GameServerCore.Domain.GameObjects;
+using GameServerCore.Domain.GameObjects.Spell;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI
+{
+    public class Pet : Minion, IPet
+    {
+        public IBuff CloneBuff { get; }
+        public float LifeTime { get; }
+        public bool CloneInventory { get; }
+        public bool DoFade { get; }
+        public bool ShowMinimapIfClone { get; }
+        public Pet(
+            Game game,
+            IChampion owner,
+            ISpell spell,
+            Vector2 position,
+            string name,
+            string model,
+            string buffName,
+            float lifeTime,
+            bool cloneInventory = true,
+            bool showMinimapIfClone = true,
+            bool doFade = false
+            ) : base(game, owner, position, model, name, team: owner.Team)
+        {
+            CloneBuff = new Buff(game, buffName, lifeTime, 1, spell, owner, owner);
+            AddBuff(CloneBuff);
+            ShowMinimapIfClone = showMinimapIfClone;
+            DoFade = doFade;
+            LifeTime = lifeTime;
+            CloneInventory = cloneInventory;
+            
+            //This actually seems to be handled on-script, since Tibbers has CloneInventory set as true, but doesn't actually clone the inventory
+            /*if (CloneInventory)
+            {
+                foreach(var item in owner.Inventory.GetAllItems())
+                {
+                    Inventory.AddItemToSlot(item.ItemData, this, owner.Inventory.GetItemSlot(item));
+                }
+            }*/
+            game.ObjectManager.AddObject(this);
+        }
+
+    }
+}
