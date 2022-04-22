23b8dd1cb5f27f38baed8766d2293a07869e6512
T1ti <40864460+T1ti@users.noreply.github.com>
2022-04-22 21:07:18 +0200

    use existing function to get enemy team

--- GameServerLib/GameObjects/AttackableUnits/AttackableUnit.cs
+++ GameServerLib/GameObjects/AttackableUnits/AttackableUnit.cs
@@ -700,13 +700,7 @@ namespace LeagueSandbox.GameServer.GameObjects.AttackableUnits
             {
                 if (!(obj is IMonster))
                 {
-                    var enemyTeam = TeamId.TEAM_BLUE;
-                    if (Team == TeamId.TEAM_BLUE)
-                    {
-                        enemyTeam = TeamId.TEAM_PURPLE;
-                    }
-
-                    var champs = _game.ObjectManager.GetChampionsInRangeFromTeam(Position, _game.Map.MapScript.MapScriptMetadata.AIVars.EXPRadius, enemyTeam, true);
+                    var champs = _game.ObjectManager.GetChampionsInRangeFromTeam(Position, _game.Map.MapScript.MapScriptMetadata.AIVars.EXPRadius, CustomConvert.GetEnemyTeam(Team), true);
                     if (champs.Count > 0)
                     {
                         var expPerChamp = obj.Stats.ExpGivenOnDeath.Total / champs.Count;
