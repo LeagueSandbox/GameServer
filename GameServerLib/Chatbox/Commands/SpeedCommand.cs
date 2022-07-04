cc6356db9b04c9552aeb2425712c4174fbfbc617
cabeca143 <72713387+cabeca1143@users.noreply.github.com>
2022-07-04 18:44:28 -0300

    Map11 NullReference Micro Fix (#1484)
    * Fixed Map11's script mentioning an uninitialized List variable.
    * Reworked a bit how the speed command works, now instead of typing `!speed [f or p] [ammount]`, you just have to type `!speed [f ammount] [p ammount]`. 
    This resembles how it used to work if you just wanted flat move speed, but now with the optional extra parameter for the percent move speed

--- GameServerLib/Chatbox/Commands/SpeedCommand.cs
+++ GameServerLib/Chatbox/Commands/SpeedCommand.cs
@@ -9,7 +9,7 @@ namespace LeagueSandbox.GameServer.Chatbox.Commands
         private readonly IPlayerManager _playerManager;
 
         public override string Command => "speed";
-        public override string Syntax => $"{Command} parameter [p (percent), f (flat)] speed";
+        public override string Syntax => $"{Command} [flat speed] [percent speed]";
 
         public SpeedCommand(ChatCommandManager chatCommandManager, Game game)
             : base(chatCommandManager, game)
@@ -20,7 +20,7 @@ namespace LeagueSandbox.GameServer.Chatbox.Commands
         public override void Execute(int userId, bool hasReceivedArguments, string arguments = "")
         {
             var split = arguments.ToLower().Split(' ');
-            if (split.Length < 2)
+            if (split.Length < 2 || split.Length > 3)
             {
                 ChatCommandManager.SendDebugMsgFormatted(DebugMsgType.SYNTAXERROR);
                 ShowSyntax();
@@ -30,24 +30,18 @@ namespace LeagueSandbox.GameServer.Chatbox.Commands
             {
                 IStatsModifier stat = new StatsModifier();
 
-                switch (split[1])
+                if (split.Length == 3)
                 {
-                    case "p":
-                        stat.MoveSpeed.PercentBonus = float.Parse(split[2]) / 100;
-                        break;
-                    case "f":
-                        stat.MoveSpeed.FlatBonus = float.Parse(split[2]);
-                        break;
-                    default:
-                        ChatCommandManager.SendDebugMsgFormatted(DebugMsgType.ERROR, "Incorrect parameter");
-                        break;
+                    stat.MoveSpeed.PercentBonus = float.Parse(split[2]) / 100;
                 }
+                stat.MoveSpeed.FlatBonus = float.Parse(split[1]);
 
                 _playerManager.GetPeerInfo(userId).Champion.AddStatModifier(stat);
             }
             catch
             {
-                ChatCommandManager.SendDebugMsgFormatted(DebugMsgType.ERROR, "Incorrect parameter");
+                ChatCommandManager.SendDebugMsgFormatted(DebugMsgType.SYNTAXERROR);
+                ShowSyntax();
             }
         }
     }
