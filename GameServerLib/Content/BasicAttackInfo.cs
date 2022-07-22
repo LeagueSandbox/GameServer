91fd5690f16fd6cf36a8d094b14e231dd40ee1fc
cabeca143 <72713387+cabeca1143@users.noreply.github.com>
2022-07-21 23:32:16 -0300

    Moved BasicAttackInfo

---
+++ GameServerLib/Content/BasicAttackInfo.cs
@@ -0,0 +1,15 @@
+﻿using GameServerCore.Domain.GameObjects.Spell;
+
+namespace GameServerLib.Content
+{
+    public class BasicAttackInfo : IBasicAttackInfo
+    {
+        public string Name { get; init; }
+        public float AttackCastTime { get; init; }
+        public float AttackDelayCastOffsetPercent { get; init; }
+        public float AttackDelayCastOffsetPercentAttackSpeedRatio { get; init; }
+        public float AttackDelayOffsetPercent { get; init; }
+        public float AttackTotalTime { get; init; }
+        public float Probability { get; init; }
+    }
+}
--- GameServerLib/Content/CharData.cs
+++ GameServerLib/Content/CharData.cs
@@ -257,17 +257,6 @@ namespace LeagueSandbox.GameServer.Content
 
             return this;
         }
-
-        public class BasicAttackInfo : IBasicAttackInfo
-        {
-            public string Name { get; init; }
-            public float AttackCastTime { get; init; }
-            public float AttackDelayCastOffsetPercent { get; init; }
-            public float AttackDelayCastOffsetPercentAttackSpeedRatio { get; init; }
-            public float AttackDelayOffsetPercent { get; init; }
-            public float AttackTotalTime { get; init; }
-            public float Probability { get; init; }
-        }
     }
 }
 
