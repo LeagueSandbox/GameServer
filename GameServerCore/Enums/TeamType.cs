0a385c39628edf2f1e3a9dc4723f8bdcf04ed697
cabeca143 <72713387+cabeca1143@users.noreply.github.com>
2022-07-28 02:41:30 -0300

    Create TeamType.cs

---
+++ GameServerCore/Enums/TeamType.cs
@@ -0,0 +1,12 @@
+﻿namespace GameServerCore.Enums
+{
+    public enum TeamType : uint
+    {
+        ALL = 0xFFFFFFFE,
+        NONE = 0xFFFFFFFF,
+        ORDER = 0x0,
+        CHAOS = 0x1,
+        NEUTRAL = 0x2,
+        SPECIFIC = 0x3,
+    }
+}
